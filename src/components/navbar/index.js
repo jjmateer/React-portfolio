c233e60b3fd31299c2252736a20fe38a5a1c0be1
John Mateer <jj.mateer@live.com>
2019-11-07 14:57:37 -0800

    Added icons and logo to side navbar.

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -6,19 +6,20 @@ class Navbar extends Component {
     render() {
         return (
             <div id="sidenav">
-                <div id="logo" />
+                <Link id="logo" to="/">jpm</Link>
                 <div id="siteLinks">
-                    <Link className="siteLink" to="/">Home</Link>
-                    <Link className="siteLink" to="/">About</Link>
-                    <Link className="siteLink" to="/">Projects</Link>
-                    <Link className="siteLink" to="/">Contact</Link>
-                </div>
+                    <Link id="link1" className="fa fa-fw fa-home" to="/"></Link>
+                    <Link id="link2" className="fa fa-fw fa-user" to="/"></Link>
+                    <Link id="link3" className="fa fa-briefcase" to="/" ></Link>
+                    <Link id="link4" className="fa fa-fw fa-envelope" to="/" ></Link >
+                </div >
+
                 <div id="resourceLinks">
-                    <Link className="resourceLink" to="/">Github</Link>
-                    <Link className="resourceLink" to="/">Linkedin</Link>
-                    <Link className="resourceLink" to="/">Resume</Link>
+                    <Link id="link5" className="fa fa-github" to="/"></Link>
+                    <Link id="link6" className="fa fa-linkedin" to="/"></Link>
+                    <Link id="link7" className="fa fa-file-text" to="/"></Link>
                 </div>
-            </div>
+            </div >
         )
     }
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Navbar from "../components/navbar"
 
 class Home extends Component {
     render() {

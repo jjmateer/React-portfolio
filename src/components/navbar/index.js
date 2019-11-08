0ab5455f051c0a3f272054b96e0e916301c7b0be
John Mateer <jj.mateer@live.com>
2019-11-08 13:48:24 -0800

    Started building top navbar for mobile, in process of implementing react-burger-menu.

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { slide as Menu } from 'react-burger-menu'
 import "./style.css";
 
 class Navbar extends Component {
+    showSettings(event) {
+        event.preventDefault();
+    }
     render() {
-        return (
+        return (<div>
             <div id="sidenav">
                 <Link id="logo" to="/">jpm</Link>
                 <div id="siteLinks">
@@ -20,6 +24,19 @@ class Navbar extends Component {
                     <Link id="link7" className="fa fa-file-text" to="/"> </Link>
                 </div>
             </div >
+            <div id="topnav">
+                <Link id="logo" to="/">jpm</Link>
+                <Menu noOverlay id="hamburger">
+                    <Link id="link1top" className="fa fa-fw fa-home" to="/"></Link>
+                    <Link id="link2top" className="fa fa-fw fa-user" to="/about"></Link>
+                    <Link id="link3top" className="fa fa-briefcase" to="/projects" ></Link>
+                    <Link id="link4top" className="fa fa-fw fa-envelope" to="/contact" ></Link >
+                    <a id="link5top" className="fa fa-github" href="https://github.com/jjmateer"> </a>
+                    <a id="link6top" className="fa fa-linkedin" href="https://linkedin.com/in/john-mateer"> </a>
+                    <Link id="link7top" className="fa fa-file-text" to="/"> </Link>
+                </Menu>
+            </div >
+        </div>
         )
     }
 }

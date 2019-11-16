141cb928dc727b59577b4d2fe6a0ca94bee7aa73
John Mateer <jj.mateer@live.com>
2019-11-16 12:35:50 -0800

    Added pictures from github pages and adjusted sizing for project slides.

--- src/components/carousel/carouselItem.js
+++ src/components/carousel/carouselItem.js
@@ -5,11 +5,12 @@ function CarouselItem(props) {
     return (
         <div id="project">
             <h2 className="project-name">{props.title}</h2>
-            <div id="link-container">
-                <a className="code-link" href={props.codelink}>Code</a>
-                <a className="demo-link" href={props.demolink}>Demo</a>
-            </div>
             <div className="description">{props.description}</div>
+            <div id="link-container">
+                {props.codelink ? <a className="code-link" href={props.codelink}>Code</a> : <p className="code-link">No code available.</p>}
+                {props.demolink ? <a className="demo-link" href={props.demolink}>Demo</a> : <p className="demo-link">No demo available.</p>}
+            </div>
+            {/* <div className="description-mobile">{props.description}</div> */}
             <img className="project-image" src={props.image} alt={props.image}></img>
         </div>
     )

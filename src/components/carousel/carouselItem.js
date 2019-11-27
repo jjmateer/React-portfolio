7f6225c231a89e35be294a9c19d3db8395860022
John Mateer <jj.mateer@live.com>
2019-11-26 23:18:47 -0800

    Put buttons above description in carousel.

--- src/components/carousel/carouselItem.js
+++ src/components/carousel/carouselItem.js
@@ -5,11 +5,11 @@ function CarouselItem(props) {
     return (
         <div id="project">
             <h2 className="project-name">{props.title}</h2>
-            <div className="description">{props.description}</div>
             <div id="link-container">
                 {props.codelink ? <a className="code-link" href={props.codelink}>Code</a> : <p className="code-linkNA">N/A</p>}
                 {props.demolink ? <a className="demo-link" href={props.demolink}>Demo</a> : <p className="demo-linkNA">N/A</p>}
             </div>
+            <div className="description">{props.description}</div>
             {/* <div className="description-mobile">{props.description}</div> */}
             <img className="project-image" src={props.image} alt={props.image}></img>
         </div>

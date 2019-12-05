eff6c861935654b1606a9ae99cfafcf3ffeb4fb6
John Mateer <jj.mateer@live.com>
2019-12-04 23:18:20 -0800

    Added margin for portrait screen sizes, took off other animation for technicall skills.

--- src/components/intro/index.js
+++ src/components/intro/index.js
@@ -32,15 +32,18 @@ export default class Intro extends React.Component {
         return (
             <AbsoluteWrap>
                 <div id="intro">
-                    <h1 className="mainH">Hello, my name is John Mateer.</h1>
+                    <div id="tone1">
+                    <h1 className="mainH" style={{color:"white"}}>Hello, my name is John Mateer.</h1>
                     <div id="introp">I am a fast-learning full-stack web developer from Roseville, CA looking to move up the tech industry.
+                    <br/>I have a solid understanding of javascript and love working with the MERN stack.
                     <br /> Feel free to check out some of my <Link id="highlightedWord" to="/projects">projects</Link>.
                     <br />If you would like to get in contact, send a <Link id="highlightedWord" to="/contact">message</Link> or email me at jj.mateer@live.com. </div>
-                    <h1 id="tsheader" className="mainH"><RandomText text={"Technical Skills"} /></h1>
+                    <h1 id="tsheader" className="mainH"><WaveText text={"Technical Skills"} /></h1>
+                    </div>
                     <ul className="tSkills">
                         <div>
                             {this.state.skillsArr.map(skill => (
-                                <li key={skill.key} className="skill"><RandomText
+                                <li key={skill.key} className="skill"><WaveText
                                     text={skill.skill}
                                 />
                                 </li>
@@ -48,7 +51,7 @@ export default class Intro extends React.Component {
                         </div>
                         <div>
                             {this.state.skillsArr2.map(skill => (
-                                <li key={skill.key} className="skill"><RandomText
+                                <li key={skill.key} className="skill"><WaveText
                                     text={skill.skill}
                                 />
                                 </li>

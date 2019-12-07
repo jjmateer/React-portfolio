43259e08f5866892ce9360b56e113bfdd0e779e1
John Mateer <jj.mateer@live.com>
2019-12-06 19:31:06 -0800

    commit

--- src/components/emailform/index.js
+++ src/components/emailform/index.js
@@ -6,7 +6,6 @@ function EmailForm() {
     return (
         <AbsoluteWrap>
             <form id="fs-frm" name="simple-contact-form" acceptCharset="utf-8" action="https://formspree.io/jj.mateer@live.com" method="post">
-                <h1 className="contactLabels" style={{textDecoration:"underline"}}>Message me!</h1>
                 <label className="contactLabels" htmlFor="full-name">Full Name</label>
                 <input type="text" name="name" id="full-name" placeholder="First and Last" required></input>
                 <label className="contactLabels" htmlFor="email-address">Email Address</label>
@@ -15,7 +14,7 @@ function EmailForm() {
                 <textarea rows="5" name="message" id="message" placeholder="Your message here..." required></textarea>
                 <input type="hidden" name="_subject" id="email-subject" value="Contact Form Submission"></input>
                 <input type="hidden" name="_next" value="https://react-slick.neostack.com/docs/example/custom-arrows/" />
-                <input type="submit" id="submitBtn" value="Submit"></input>
+                <input type="submit" id="submitBtn" value="Send Message"></input>
             </form>
         </AbsoluteWrap>
     )
